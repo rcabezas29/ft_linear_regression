db2b9704cdf8891908ee3daf87fb8026c793a8a7
Raul Cabezas gonzalez <raul.29cg@gmail.com>
2022-12-22 15:10:06 +0100

    Finishing linear regression

--- predict.py
+++ predict.py
@@ -4,14 +4,16 @@ def catch_tethas():
             t1, t2 = file.read().split(',')
             return float(t1), float(t2)
     except:
-        print('.theta file does not exists or it has an error. Thetas set to 0')
         return 0.0, 0.0
 
+def	estimate_price(mileage):
+	theta0, theta1 = catch_tethas()
+	return theta0 + (theta1 * mileage)
+
 if __name__ == '__main__':
-    theta0, theta1 = catch_tethas()
     try:
         mil = float(input('Input a mileage: '))
     except:
         print('Wrong input')
     else:
-        print(f'The expected cost for that mileage is {theta0 + (theta1 * mil)}')
+        print(f'The expected cost for that mileage is {estimate_price(mil)}')
--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from predict import estimate_price, catch_tethas
 
-if __name__ == '__main__':
-	data = pd.read_csv("./data.csv")
+def	show_graphic(data):
 	km = list(data.km)
 	price = list(data.price)
 
@@ -10,6 +10,46 @@ if __name__ == '__main__':
 	plt.xlabel("KM")
 	plt.ylabel("Price")
 	plt.show()
-	# df = pd.DataFrame(data)
 
-	# df.plot.scatter(x="km", y="price")
+def	get_function_slope(data):
+	sum = 0
+	m = 0
+	for index, row in data.iterrows():
+		km = row.km
+		price = row.price
+		sum += (estimate_price(km) - price) * km
+		m += 1
+	
+	tmp_theta1 = sum / m * 0.5
+	return tmp_theta1
+
+def	get_function_constant(data) -> float:
+	sum = 0
+	m = 0
+	for index, row in data.iterrows():
+		km = row.km
+		price = row.price
+		sum += (estimate_price(km) - price)
+		m += 1
+	
+	tmp_theta0 = sum / m * 0.5
+	return tmp_theta0
+	
+
+def	linear_regression(data):
+	k = get_function_constant(data)
+	slope = get_function_slope(data)
+	with open('.theta', 'w') as f:
+		f.write(f'{k},{slope}')
+		f.close()
+
+
+if __name__ == '__main__':
+	data = pd.read_csv("./data.csv")
+
+	for x in range(1000):
+		linear_regression(data)
+	show_graphic(data)
+
+
+
