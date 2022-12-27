bc61502341ce710566e327a7d67ab3079ef88292
Raul Cabezas gonzalez <raul.29cg@gmail.com>
2022-12-27 16:19:18 +0100

    Implementing gradient descent

--- train.py
+++ train.py
@@ -8,51 +8,47 @@ def	show_graphic(data, theta0, theta1):
 	price = list(data.price)
 
 	plt.scatter(km, price)
-	x = np.linspace(61789, 240000, 100)
+	x = np.linspace(22899, 240000, 100)
 	y = theta1 * x + theta0
 	plt.plot(x, y, '-r', label='y = mx + k')
 	plt.xlabel("KM")
 	plt.ylabel("Price")
 	plt.show()
 
-def	normalize(data, t0, t1):
-	X = data['km'].tolist()
-	Y = data['price'].tolist()
+def	normalize(data):
+	normalized_df = (data-data.min()) / (data.max() - data.min())
+	return(normalized_df)
 
-	norm_t0 = (t0 - min(X)) / (max(X) - min(X))
-	norm_t1 = (t1 - min(Y)) / (max(Y) - min(Y))
+def	cost_function(kms, prices):
+	m = kms.size
+	x =  0.1 * (1 / m) * sum(estimate_price(kms) - prices)
+	y = 0.1 * (1 / m) * sum((estimate_price(kms) - prices) * kms)
 
-	return norm_t0, norm_t1
+	return x, y
+
 
 def	linear_regression(data):
-	t0_sum = 0
-	t1_sum = 0
-	m = 0
+	data = normalize(data)
+	X = data.iloc[:, 0]
+	Y = data.iloc[:, 1]
 
-	for index, row in data.iterrows():
-		km = row.km
-		price = row.price
+	theta0, theta1 = catch_tethas()
 
-		km, price = normalize(data, km, price)
+	for _ in range(1000):
+		tmp_theta0, tmp_theta1 = cost_function(X, Y)
+		theta0 -= tmp_theta0
+		theta1 -= tmp_theta1
 
-		t0_sum += (estimate_price(km) - price)
-		t1_sum += (estimate_price(km) - price) * km
+		# error = cost_function(kms, prices)
 
-		m += 1
-	
-	tmp_theta0 = t0_sum / m * 0.1
-	tmp_theta1 = t1_sum / m * 0.1
-
-	return tmp_theta0, tmp_theta1
-
+		with open('.theta', 'w') as f:
+			f.write(f'{theta0},{theta1}')
+			f.close()
+	return theta0, theta1
 
 if __name__ == '__main__':
 	data = pd.read_csv("./data.csv")
 
-	for x in range(1000):
-		theta0, theta1 = linear_regression(data)
-		with open('.theta', 'w') as f:
-			f.write(f'{theta0},{theta1}')
-			f.close()
-		show_graphic(data, theta0, theta1)
+	theta0, theta1 = linear_regression(data)
+	show_graphic(data, theta0, theta1)
 
