9ca82dcc049dec97493ff510472b5aea6ec641d5
Raul Cabezas gonzalez <raul.29cg@gmail.com>
2022-12-27 11:31:06 +0100

    Normalized values

--- train.py
+++ train.py
@@ -1,55 +1,58 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from predict import estimate_price, catch_tethas
+import numpy as np
 
-def	show_graphic(data):
+def	show_graphic(data, theta0, theta1):
 	km = list(data.km)
 	price = list(data.price)
 
 	plt.scatter(km, price)
+	x = np.linspace(61789, 240000, 100)
+	y = theta1 * x + theta0
+	plt.plot(x, y, '-r', label='y = mx + k')
 	plt.xlabel("KM")
 	plt.ylabel("Price")
 	plt.show()
 
-def	get_function_slope(data):
-	sum = 0
-	m = 0
-	for index, row in data.iterrows():
-		km = row.km
-		price = row.price
-		sum += (estimate_price(km) - price) * km
-		m += 1
-	
-	tmp_theta1 = sum / m * 0.5
-	return tmp_theta1
+def	normalize(data, t0, t1):
+	X = data['km'].tolist()
+	Y = data['price'].tolist()
 
-def	get_function_constant(data) -> float:
-	sum = 0
-	m = 0
-	for index, row in data.iterrows():
-		km = row.km
-		price = row.price
-		sum += (estimate_price(km) - price)
-		m += 1
-	
-	tmp_theta0 = sum / m * 0.5
-	return tmp_theta0
-	
+	norm_t0 = (t0 - min(X)) / (max(X) - min(X))
+	norm_t1 = (t1 - min(Y)) / (max(Y) - min(Y))
+
+	return norm_t0, norm_t1
 
 def	linear_regression(data):
-	k = get_function_constant(data)
-	slope = get_function_slope(data)
-	with open('.theta', 'w') as f:
-		f.write(f'{k},{slope}')
-		f.close()
+	t0_sum = 0
+	t1_sum = 0
+	m = 0
+
+	for index, row in data.iterrows():
+		km = row.km
+		price = row.price
+
+		km, price = normalize(data, km, price)
+
+		t0_sum += (estimate_price(km) - price)
+		t1_sum += (estimate_price(km) - price) * km
+
+		m += 1
+	
+	tmp_theta0 = t0_sum / m * 0.1
+	tmp_theta1 = t1_sum / m * 0.1
+
+	return tmp_theta0, tmp_theta1
 
 
 if __name__ == '__main__':
 	data = pd.read_csv("./data.csv")
 
 	for x in range(1000):
-		linear_regression(data)
-	show_graphic(data)
-
-
+		theta0, theta1 = linear_regression(data)
+		with open('.theta', 'w') as f:
+			f.write(f'{theta0},{theta1}')
+			f.close()
+		show_graphic(data, theta0, theta1)
 
